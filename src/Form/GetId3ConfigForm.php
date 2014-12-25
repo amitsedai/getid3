1310625a3875e5e10b8d595ef53163a739de4411
Amit Sedai <amitsedaiz@gmail.com>
2014-12-25 16:02:15 +0530

    Changes for Config and other issues

--- src/Form/GetId3ConfigForm.php
+++ src/Form/GetId3ConfigForm.php
@@ -11,12 +11,12 @@ use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Component\Utility\String;
 
-class GetID3ConfigForm  extends  ConfigFormBase {
+class GetId3ConfigForm  extends  ConfigFormBase {
 
   /**
    * {@inheritdoc}
    */
-  public function getFormID() {
+  public function getFormId() {
     return 'getid3_systemconfigformbase';
   }
 
@@ -24,26 +24,23 @@ class GetID3ConfigForm  extends  ConfigFormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $form = parent::buildForm($form, $form_state);
-
     $path = getid3_get_path();
     $form['getid3_path'] = array(
       '#type' => 'textfield',
-      '#title' => t('Path'),
+      '#title' => $this->t('Path'),
       '#default_value' => $path,
       '#disabled' => TRUE,
-      '#description' => t('The location where getID3() is installed. Relative paths are from the Drupal root directory.'),
+      '#description' => $this->t('The location where getID3() is installed. Relative paths are from the Drupal root directory.'),
     );
 
     $form['getid3_path']['#after_build'][] = array(get_class($this), 'afterBuild');
 
-
     if ($version = getid3_get_version()) {
       $form['getid3_version'] = array(
         '#type' => 'item',
-        '#title' => t('Version'),
+        '#title' => $this->t('Version'),
         '#markup' => '<pre>' . String::checkPlain($version) . '</pre>',
-        '#description' => t("If you're seeing this it indicates that the getID3 library was found."),
+        '#description' => $this->t("If you're seeing this it indicates that the getID3 library was found."),
       );
 
       // Check for existence of the 'demos' folder, contained in the getID3
@@ -51,22 +48,21 @@ class GetID3ConfigForm  extends  ConfigFormBase {
       // so we recommend that the user delete it.
       $getid3_demos_path = $path . '/../demos';
       if (file_exists($getid3_demos_path)) {
-        drupal_set_message(t("Your getID3 library is insecure! The demos distributed with getID3 contains code which creates a huge security hole. Remove the demos directory (%path) from beneath Drupal's directory.", array('%path' => realpath($getid3_demos_path))), 'error');
+        drupal_set_message($this->t("Your getID3 library is insecure! The demos distributed with getID3 contains code which creates a huge security hole. Remove the demos directory (%path) from beneath Drupal's directory.", array('%path' => realpath($getid3_demos_path))), 'error');
       }
     }
-    $show_warnings = $this->configFactory->get('getid3.settings')
-      ->get('getid3_show_warnings');
+    $show_warnings = $this->config('getid3.settings')->get('getid3_show_warnings');
     if(empty($show_warnings)){
       $show_warnings = FALSE;
     }
     $form['getid3_show_warnings'] = array(
       '#type' => 'checkbox',
-      '#title' => t("Display Warnings"),
+      '#title' => $this->t("Display Warnings"),
       '#default_value' => $show_warnings,
-      '#description' => t("Check this to display the warning messages from the getID3 library when reading and writing ID3 tags. Generally it's a good idea to leave this unchecked, getID3 reports warnings for several trivial problems and the warnings can be confusing to users. This setting can be useful when debugging problems with the ID3 tags."),
+      '#description' => $this->t("Check this to display the warning messages from the getID3 library when reading and writing ID3 tags. Generally it's a good idea to leave this unchecked, getID3 reports warnings for several trivial problems and the warnings can be confusing to users. This setting can be useful when debugging problems with the ID3 tags."),
     );
-    return $form;
 
+    return parent::buildForm($form, $form_state);
   }
 
   /**
@@ -83,7 +79,7 @@ class GetID3ConfigForm  extends  ConfigFormBase {
     parent::submitForm($form, $form_state);
 
     // Save the new value.
-    $this->configFactory->get('getid3.settings')
+    $this->config('getid3.settings')
       ->set('getid3_show_warnings', $form_state->getValue('getid3_show_warnings'))
       ->save();
   }
